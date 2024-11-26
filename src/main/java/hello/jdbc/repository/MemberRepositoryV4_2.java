f33a3063dbeaf63a0c8ab24abd2a53806c7eb19e
hee <heezzanggg@gmail.com>
2024-11-26 20:53:52 +0900

    예외

--- src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV4_2.java
@@ -1,33 +1,36 @@
 package hello.jdbc.repository;
 
 import hello.jdbc.domain.Member;
-import hello.jdbc.repository.ex.MyDbException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.jdbc.support.JdbcUtils;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.jdbc.support.SQLExceptionTranslator;
 
 import javax.sql.DataSource;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-/*
-예외 누수 문제 해결
-체크예외를 런타임 예외로 변경
-MemberRepository 인터페이스 사용
-throws SQLException 제거
+/**
+ * SQLExceptionTranslator 추가
  */
 @Slf4j
-public class MemberRepositoryV4_1 implements MemberRepository {
+public class MemberRepositoryV4_2 implements MemberRepository {
 
     private final DataSource dataSource;
+    private final SQLExceptionTranslator exTranslator;
 
-    public MemberRepositoryV4_1(DataSource dataSource) {
+    public MemberRepositoryV4_2(DataSource dataSource) {
         this.dataSource = dataSource;
+        this.exTranslator = new SQLErrorCodeSQLExceptionTranslator(dataSource);
     }
 
     @Override
     public Member save(Member member) {
-        String sql = "insert into member (member_id, money) values ( ?, ?)";
+        String sql = "insert into member (member_id, money) values (?, ?)";
 
         Connection con = null;
         PreparedStatement pstmt = null;
@@ -40,7 +43,7 @@ public class MemberRepositoryV4_1 implements MemberRepository {
             pstmt.executeUpdate();
             return member;
         } catch (SQLException e) {
-            throw new MyDbException(e);
+            throw exTranslator.translate("save", sql, e);
         } finally {
             close(con, pstmt, null);
         }
@@ -60,17 +63,16 @@ public class MemberRepositoryV4_1 implements MemberRepository {
             pstmt.setString(1, memberId);
 
             rs = pstmt.executeQuery();
-
             if (rs.next()) {
                 Member member = new Member();
                 member.setMemberId(rs.getString("member_id"));
                 member.setMoney(rs.getInt("money"));
                 return member;
             } else {
-                throw new NoSuchElementException("member not found memberId =" + memberId);
+                throw new NoSuchElementException("member not found memberId = " + memberId);
             }
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw exTranslator.translate("findById", sql, e);
         } finally {
             close(con, pstmt, rs);
         }
@@ -79,8 +81,10 @@ public class MemberRepositoryV4_1 implements MemberRepository {
     @Override
     public void update(String memberId, int money) {
         String sql = "update member set money = ? where member_id = ?";
+
         Connection con = null;
         PreparedStatement pstmt = null;
+
         try {
             con = getConnection();
             pstmt = con.prepareStatement(sql);
@@ -88,38 +92,40 @@ public class MemberRepositoryV4_1 implements MemberRepository {
             pstmt.setString(2, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw exTranslator.translate("update", sql, e);
         } finally {
             close(con, pstmt, null);
         }
     }
 
     @Override
-    public void delete(String id) {
+    public void delete(String memberId) {
         String sql = "delete from member where member_id = ?";
 
         Connection con = null;
         PreparedStatement pstmt = null;
 
-        try{
+        try {
             con = getConnection();
             pstmt = con.prepareStatement(sql);
-            pstmt.setString(1, id);
+            pstmt.setString(1, memberId);
             pstmt.executeUpdate();
         } catch (SQLException e) {
-            throw new MyDbException(e);
+            throw exTranslator.translate("delete", sql, e);
+        } finally {
+            close(con, pstmt, null);
         }
+    }
+
+    private void close(Connection con, PreparedStatement pstmt, ResultSet rs) {
+        JdbcUtils.closeResultSet(rs);
+        JdbcUtils.closeStatement(pstmt);
+        DataSourceUtils.releaseConnection(con, dataSource);
     }
 
     private Connection getConnection() {
         Connection con = DataSourceUtils.getConnection(dataSource);
         log.info("get connection={} class{}", con, con.getClass());
         return con;
-    }
-
-    private void close(Connection conn, Statement stmt, ResultSet rs) {
-        JdbcUtils.closeResultSet(rs);
-        JdbcUtils.closeStatement(stmt);
-        DataSourceUtils.releaseConnection(conn, dataSource);
     }
 }
